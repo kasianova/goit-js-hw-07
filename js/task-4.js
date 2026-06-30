5c9c8d31742d491589e465082dbabd6a9720ff94
Anastasiia Kasianova <n.kostik21@gmail.com>
2026-06-30 09:26:13 +0200

    all files

--- js/task-4.js
+++ js/task-4.js
@@ -1,14 +1,20 @@
 const form = document.querySelector(".login-form");
+
 form.addEventListener("submit", handlerSubmit) ;
 
 function handlerSubmit(event) {
     event.preventDefault();
     const elements = event.target.elements;
-    for (const element of elements) {
-        if (element.tagName === "input" && element.value.trim() === "") {
+        if (!elements.email.value.trim() || !elements.password.value.trim()) {
             alert("All form fields must be filled in");
             return;
         }
-    }
+    
+const info = {
+    email: elements.email.value.trim(),
+    password: elements.password.value.trim(),
+  };
 
+  console.log(info); 
+  event.target.reset();
 }
--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,18 @@
+
+const colorSpan = document.querySelector(".color");
+const colorBtn = document.querySelector(".change-color");
+// const bodyColor = document.querySelector(".widget");
+colorBtn.addEventListener("click", handClick);
+
+function handClick() {
+
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+}
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
